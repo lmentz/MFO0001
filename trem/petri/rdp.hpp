a9d589d8379a506f9a9531e185d9b1a2a76e731e
Lucas L. Mentz <lucaslittermentz@gmail.com>
2018-11-21 09:58:20 -0200

    uh

--- trem/petri/rdp.hpp
+++ trem/petri/rdp.hpp
@@ -6,33 +6,54 @@
 
 class PetriNet {
 private:
-    std::vector<std::vector<int> > pre; // Linhas = Lugares; Colunas = Transicoes
-    std::vector<std::vector<int> > pos; // Linhas = Lugares; Colunas = Transicoes
-    std::vector<int> m; // Marcação atual
-    std::vector<std::string> lugares; // Nomes apenas
-    std::vector<std::string> transicoes; // Nomes apenas
+	std::vector<std::vector<int> > pre; // Linhas = Lugares; Colunas = Transicoes
+	std::vector<std::vector<int> > pos; // Linhas = Lugares; Colunas = Transicoes
+	std::vector<int> m; // Marcação atual
+	std::vector<std::string> lugares; // Nomes apenas
+	std::vector<std::string> transicoes; // Nomes apenas
 
 public:
-    PetriNet(std::vector<std::vector<int> > pre, std::vector<std::vector<int> > pos, std::vector<int> m0,
-             std::vector<std::string> lugares, std::vector<std::string> transicoes){
-        this->pre = pre;
-        this->pos = pos;
-        this->m = m0;
-        // Carregar nomes das coisas
-        this->lugares = lugares;
-        this->transicoes = transicoes;
-    }
+	PetriNet(std::vector<std::vector<int> > pre, std::vector<std::vector<int> > pos, std::vector<int> m0,
+			 std::vector<std::string> lugares, std::vector<std::string> transicoes){
+		this->pre = pre;
+		this->pos = pos;
+		this->m = m0;
+		// Carregar nomes das coisas
+		this->lugares = lugares;
+		this->transicoes = transicoes;
+	}
+	
+	std::string getHabilitadoTrem(std::string trem) {
+		for(int i = 0; i < transicoes.size(); i++){
+			if(transicoes[i].find(trem) != std::string::npos) {
+				bool habilitado = true;
+				
+				// mutex lock na coluna i do pre
+				for(int j = 0; j < m.size(); j++){
+					if(m[j] < pre[j][i]){
+						habilitado = false;
+						break;
+					}
+				}
+				// mutex unlock na coluna i do pre
+				
+				if (habilitado) return transicoes[i];
+			}
+		}
+		return "";
+	}
 
-    std::string proxTransicao(){
-        std::vector<int> ligados(transicoes.size(), 0);
-        for(int i = 0; i < transicoes.size(); i++){
-            bool habilitado = true;
-            for(int j = 0; j < transicoes[i].size(); j++){
-                if (m[j] < )
-            }
-        }
-    }
+	void iniciaTransicao(std::string trans){
+		// mutex lock global
+		// m = m - pre; da transição trans
+		// mutex unlock global
+	}
 
+	void finalizaTransicao(std::string trans){
+		// mutex lock global
+		// m = m + post; da transição trans
+		// mutex unlock global
+	}
 };
 
 
