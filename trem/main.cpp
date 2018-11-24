16628bb713966f8b7bc1092f6f8848a2c5803954
Lucas L. Mentz <lucaslittermentz@gmail.com>
2018-11-23 22:42:45 -0200

    Remover arquivos CMAKE, melhorias no RdP

--- trem/main.cpp
+++ trem/main.cpp
@@ -16,14 +16,54 @@ int main(int argc, char **argv) {
     
     return 0;
     */
-    
+
+    vector<vector<int> > pre =
+        {};
+    vector<vector<int> > pos =
+        {};
+    vector<int> m0 =
+        {};
+    vector<string> lugares =
+        {};
+    vector<string> transicoes =
+        {};
+
+
+    PetriNet rdp_trem(pre, pos, m0, lugares, transicoes);
+
     MapaTrem trens; 
 
     trens.Trem1Pos(1, 1);
     trens.Trem1Txt("Mensagem trem 1.");
     trens.Trem2Pos(1, 2);
     trens.Trem2Txt("Mensagem trem 2.");
-    
+
+    string t1_hab, t2_hab;
+
+    for (int i = 0; i < 1000; i++){
+        rdp_trem.lockRede();
+        t1_hab = rdp_trem.getHabilitadoTrem("t1");
+        rdp_trem.iniciaTransicao(t1_hab);
+        trens.Trem1Txt("Rodando transição " + t1_hab);
+        Thread::SleepMS(2000);
+        rdp_trem.finalizaTransicao(t1_hab);
+        trens.Trem1Txt("Finalizado transição " + t2_hab);
+        rdp_trem.unlockRede();
+
+        Thread::SleepMS(2000);
+
+        rdp_trem.lockRede();
+        t2_hab = rdp_trem.getHabilitadoTrem("t2");
+        rdp_trem.iniciaTransicao(t2_hab);
+        trens.Trem2Txt("Rodando transição " + t2_hab);
+        Thread::SleepMS(2000);
+        rdp_trem.finalizaTransicao(t2_hab);
+        trens.Trem2Txt("Finalizado transição " + t2_hab);
+        rdp_trem.unlockRede();
+        
+        Thread::SleepMS(2000);
+    }
+
     // trens.Gate(1);    
    
     // for(float p = -1; p <= 1; p += 0.01)
--- trem/petri/rdp.hpp
+++ trem/petri/rdp.hpp
@@ -3,16 +3,50 @@
 
 #include <vector>
 #include <string>
+#include <mutex>
+
+typedef unsigned int uint;
 
 class PetriNet {
 private:
-	std::vector<std::vector<int> > pre; // Linhas = Lugares; Colunas = Transicoes
-	std::vector<std::vector<int> > pos; // Linhas = Lugares; Colunas = Transicoes
-	std::vector<int> m; // Marcação atual
-	std::vector<std::string> lugares; // Nomes apenas
+	std::vector<std::vector<int> > pre;  // Linhas = Lugares; Colunas = Transicoes
+	std::vector<std::vector<int> > pos;  // Linhas = Lugares; Colunas = Transicoes
+	std::vector<int> m;                  // Marcação atual
+	std::vector<std::string> lugares;    // Nomes apenas
 	std::vector<std::string> transicoes; // Nomes apenas
+	std::vector<std::mutex> mut_trans;   // Mutex de transição (coluna no pre/pos)
+	std::vector<std::mutex> mut_lugar;   // Mutex de lugar (linha no pre/pos)
+	std::mutex mut_global;               // Mutex global da rede
 
-public:
+	int findTransicao(std::string trans){
+		for (uint i = 0; i < transicoes.size(); i++){
+			if (trans == transicoes[i]) return int(i);
+		}
+		return -1;
+	}
+
+	bool getTransacaoHabilitada(std::string trans)
+	{
+		int i = findTransicao(trans);
+		bool habilitado = true;
+		// mutex lock na coluna i do pre
+		mut_trans[i].lock();
+		for (uint j = 0; j < m.size(); j++)
+		{
+			// mut_lugar[j].lock();
+			if (m[j] < pre[j][i])
+			{
+				habilitado = false;
+				break;
+			}
+			// mut_lugar[j].unlock();
+		}
+		mut_trans[i].unlock(); // mutex unlock na coluna i do pre
+
+		return habilitado;
+	}
+
+  public:
 	PetriNet(std::vector<std::vector<int> > pre, std::vector<std::vector<int> > pos, std::vector<int> m0,
 			 std::vector<std::string> lugares, std::vector<std::string> transicoes){
 		this->pre = pre;
@@ -21,21 +55,34 @@ public:
 		// Carregar nomes das coisas
 		this->lugares = lugares;
 		this->transicoes = transicoes;
+		this->mut_trans = std::vector<std::mutex> (transicoes.size());
+		this->mut_lugar = std::vector<std::mutex> (lugares.size());
 	}
-	
+
+	void lockRede(){
+		mut_global.lock();
+	}
+
+	void unlockRede(){
+		mut_global.unlock();
+	}
+
 	std::string getHabilitadoTrem(std::string trem) {
-		for(int i = 0; i < transicoes.size(); i++){
-			if(transicoes[i].find(trem) != std::string::npos) {
+		for (uint i = 0; i < transicoes.size(); i++){
+			if (transicoes[i].find(trem) != std::string::npos) {
 				bool habilitado = true;
 				
 				// mutex lock na coluna i do pre
-				for(int j = 0; j < m.size(); j++){
-					if(m[j] < pre[j][i]){
+				// mut_trans[i].lock();
+				for (uint j = 0; j < m.size(); j++){
+					// mut_lugar[j].lock();
+					if (m[j] < pre[j][i]){
 						habilitado = false;
 						break;
 					}
+					// mut_lugar[j].unlock();
 				}
-				// mutex unlock na coluna i do pre
+				// mut_trans[i].unlock(); // mutex unlock na coluna i do pre
 				
 				if (habilitado) return transicoes[i];
 			}
@@ -43,16 +90,42 @@ public:
 		return "";
 	}
 
-	void iniciaTransicao(std::string trans){
+	int iniciaTransicao(std::string trans){
+		int ret = 0;
 		// mutex lock global
+		// mut_global.lock();
 		// m = m - pre; da transição trans
+		int i = findTransicao(trans);
+		if (i == -1 || !getTransacaoHabilitada(trans)) // Falha
+			ret = 1;
+		else {
+			for (uint j = 0; j < lugares.size(); j++){
+				m[j] -= pre[j][i];
+			}
+		}
 		// mutex unlock global
+		// mut_global.unlock();
+		return ret;
 	}
 
-	void finalizaTransicao(std::string trans){
+	int finalizaTransicao(std::string trans){
+		int ret = 0;
 		// mutex lock global
+		// mut_global.lock();
 		// m = m + post; da transição trans
+		int i = findTransicao(trans);
+		if (i == -1 || !getTransacaoHabilitada(trans)) // Falha
+			ret = 1;
+		else
+		{
+			for (uint j = 0; j < lugares.size(); j++)
+			{
+				m[j] += pos[j][i];
+			}
+		}
 		// mutex unlock global
+		// mut_global.unlock();
+		return ret;
 	}
 };
 
