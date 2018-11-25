330d650cb84b645fa7fa5a990f12fd008c1ad069
Lucas L. Mentz <lucaslittermentz@gmail.com>
2018-11-25 01:14:04 -0200

    Toques finais

--- trem/main.cpp
+++ trem/main.cpp
@@ -14,10 +14,13 @@ int main(int argc, char **argv){
 	RDPTrens tdi; // Contém as definições da rede de petri
 	PetriNet rdp_trem(tdi.pre, tdi.pos, tdi.m0,
 					  tdi.lugares, tdi.transicoes);
-	MapaTrem trens;
+	MapaTrem trens; // Controle da interface
 
-	Trem t1("t1", &rdp_trem, &trens);
-	Trem t2("t2", &rdp_trem, &trens);
+	trens.Trem1Txt("Acione o trem 1 com a tecla M");
+	trens.Trem2Txt("Acione o trem 2 com a tecla G");
+
+	Trem t1("T1", &rdp_trem, &trens); 
+	Trem t2("T2", &rdp_trem, &trens);
 
 	// cout << &trens << endl
 	// 	 << &rdp_trem << endl
@@ -25,49 +28,8 @@ int main(int argc, char **argv){
 	// 	 << &t2 << endl << endl;
 
 	thread t1_thread(&Trem::startWorker, &t1);
-	Thread::SleepMS(10);
+	// Thread::SleepMS(10);
 	thread t2_thread(&Trem::startWorker, &t2);
-
-	// trens.Gate(1);
-   
-	// for(float p = -1; p <= 1; p += 0.01)
-	// {
-	//     if(p < 0)
-	//         trens.Trem1Pos((-1) * p, 1);
-	//     else
-	//         trens.Trem1Pos(p, 0);
-	//     //trens.Trem1Pos(p.x,p.y);
-	//     Thread::SleepMS(10);
-		
-	//     trens.Trem1Txt("a1=" + to_string((int) trens.A1()) + " a2=" + to_string((int) trens.A2()) + " b1=" + to_string((int) trens.B1()) + " b2=" + to_string((int) trens.B2()) + " c=" + to_string((int) trens.C()));
-		
-	//     int key = trens.GetLastKey();
-	//     cout << "LastKey = " << key << endl;
-	//     if(key == 27)
-	//         return 1;
-	// }
-	
-	// trens.Trem1Txt("Mensagem trem 1.");
-	
-	// trens.Gate(0);
-	// for(float p = -1; p <= 1; p += 0.01)
-	// {
-	//     if(p < 0)
-	//         trens.Trem2Pos((-1) * p, 2);
-	//     else
-	//         trens.Trem2Pos(p, 0);
-	//     //trens.Trem1Pos(p.x,p.y);
-	//     Thread::SleepMS(10);
-		
-	//     trens.Trem2Txt("a1=" + to_string((int) trens.A1()) + " a2=" + to_string((int) trens.A2()) + " b1=" + to_string((int) trens.B1()) + " b2=" + to_string((int) trens.B2()) + " c=" + to_string((int) trens.C()));
-		
-	//     int key = trens.GetLastKey();
-	//     cout << "LastKey = " << key << endl;
-	//     if(key == 27)
-	//         return 1;
-	// }
-	
-	// trens.Trem2Txt("Mensagem trem 2.");
 
 	t1_thread.join();
 	t2_thread.join();
--- trem/trens/tremthread.hpp
+++ trem/trens/tremthread.hpp
@@ -3,11 +3,9 @@
 // hurrrr
 
 // #include <vector>
-// #include <mutex>
-// #include <sstream>
 #include <string>
 #include <thread>
-#include <queue>
+#include <chrono>
 #include "../petri/rdp.hpp"
 
 typedef unsigned int uint;
@@ -20,20 +18,18 @@ class Trem {
 	double tremPos;
 	double tremSpeed;
 	std::string ordem;
-	// std::queue<int> fila_entrada;
-	// std::queue<std::string> fila_estado;
 
 	void tremPrint(std::string txt)
 	{
-		if (nome == "t1")
+		if (nome == "T1")
 			trens->Trem1Txt(txt);
-		else if (nome == "t2")
+		else if (nome == "T2")
 			trens->Trem2Txt(txt);
 	}
 
 	void updatePosTrem()
 	{
-		if (nome == "t1"){
+		if (nome == "T1"){
 			if (tremPos < 0){
 				trens->Trem1Pos((-1) * tremPos, 1);
 			}
@@ -41,136 +37,372 @@ class Trem {
 				trens->Trem1Pos(tremPos, 0);
 			}
 		}
-		else if (nome == "t2")
-		{
-			if (tremPos < 0)
-			{
+		else if (nome == "T2"){
+			if (tremPos < 0){
 				trens->Trem2Pos((-1) * tremPos, 2);
 			}
-			else
-			{
+			else{
 				trens->Trem2Pos(tremPos, 0);
 			}
 		}
+		// printf("%s speed = %f\n", nome.data(), tremSpeed);
 	}
 
 	void tremRunner()
 	{
-		double accel = 0.0001;
+		double accel = 0.0001; // units/second²
+		double max_speed = 0.001;
+		auto start = std::chrono::high_resolution_clock::now(), finish = start;
+
 		while (true)
 		{
+			finish = std::chrono::high_resolution_clock::now();
+			std::chrono::duration<double> elapsedTime = finish - start;
+			start = std::chrono::high_resolution_clock::now();
+
+			double dif = accel * elapsedTime.count();
 			if (ordem == "r")
 			{
-				tremSpeed = MAX(tremSpeed + accel, 1);
+				tremSpeed = MIN(tremSpeed + dif, max_speed);
 			}
 			else if (ordem == "l")
 			{
-				tremSpeed = MIN(tremSpeed - accel, -1);
+				tremSpeed = MAX(tremSpeed - dif, -max_speed);
 			}
 			else if (ordem == "s")
 			{
 				if (tremSpeed > 0)
 				{
-					tremSpeed = MAX(tremSpeed - accel, 0);
+					tremSpeed = MAX(tremSpeed - 1.7*dif, 0);
 				}
 				else if (tremSpeed < 0)
 				{
-					tremSpeed = MIN(tremSpeed + accel, 0);
+					tremSpeed = MIN(tremSpeed + 1.7*dif, 0);
+				}
+			}
+			else if (ordem == "s-d")
+			{
+				if (tremSpeed > 0)
+				{
+					tremSpeed = MAX(tremSpeed - 0.74*dif, 0);
+				}
+			}
+			else if (ordem == "s-e")
+			{
+				if (tremSpeed < 0)
+				{
+					tremSpeed = MIN(tremSpeed + 1.18*dif, 0);
 				}
 			}
 			tremPos += tremSpeed;
+			tremPos = MIN(1, MAX(-1, tremPos));
 			updatePosTrem();
-			Thread::SleepMS(5);
+			Thread::SleepMS(20);
 		}
 	}
 
 	void rodaTrans(std::string trans)
 	{
-		if (trans == "t1-t0")
+		if (trans == "T1-t0")
 		{ // Partir de Lucerne
-			tremPrint("Saindo da estação");
-			ordem = "r";
-			Thread::SleepMS(5000);
+			tremPrint("Saindo da estacao Lucerne");
+			Thread::SleepMS(rand() % 1000);
+			this->ordem = "r";
+			Thread::SleepMS(1000);
 		}
 		
-		else if (trans == "t1-t1")
+		else if (trans == "T1-t1")
 		{ // Aguardar B1 ligar
 			while (true)
 			{
-				tremPrint("Em trânsito");
+				if (trens->A1())
+				{
+					tremPrint("Em transito");
+					break;
+				}
+				Thread::SleepMS(1); // 25 é muito, ele perde o sensor as vezes.
+			}
+			while (true)
+			{
 				if (trens->B1())
 				{
 					tremPrint("Aproximando de Stans");
 					break;
 				}
-				Thread::SleepMS(25);
+				Thread::SleepMS(1); // 25 é muito, ele perde o sensor as vezes.
 			}
 		}
-		else if (trans == "t1-t2")
+		
+		else if (trans == "T1-t2")
 		{ // T1 prossegue por Stans sem parar
+			trens->Gate(1);
+			tremPrint("Caminho livre, prosseguir");
+			// Thread::SleepMS(1000);
+			//nao pode ter tempo pq senao buga
 		}
-		else if (trans == "t1-t3")
-		{ // T1 para em Stans
+
+		else if (trans == "T1-t3")
+		{ // Parar trem 1
+			this->ordem = "s";
+			tremPrint("Parar, caminho em uso");
+			Thread::SleepMS(2000); //2000ms? éh
+			//nao pode ter tempo pq senao buga
 		}
-		else if (trans == "t1-t4")
+
+		else if (trans == "T1-t4")
+		{ // T1 pega Mutex (vai para estado T1 verificar caminho)
+			trens->Gate(1);
+			tremPrint("Seguir, caminho liberado");
+			this->ordem = "r";
+			//nao pode ter tempo pq senao buga
+		}
+
+		else if (trans == "T1-t5")
+		{ // T1 seguir pelo gate
+			while (tremPos < 0) {
+				Thread::SleepMS(100);
+			}
+			tremPrint("Seguindo viagem");
+		}
+		
+		else if (trans == "T1-t6")
+		{ // T1 apertar m1 para saida '-'
+			while (true){
+				// printf("%d %d\n", 'm', trens->GetLastKey());
+				if (trens->GetLastKey() == 'm'){
+					tremPrint("Ultima chamada (embarque)");
+					trens->GetLastKeyAndEmpty(); // limpar
+					break;
+				}
+				Thread::SleepMS((rand() % 10) + 5);
+			}
+			Thread::SleepMS(4000);
+			tremPrint("T1 partindo");
+			this->ordem = "r";
+		}
+
+		else if (trans == "T1-t7")
+		{ // sensor C=1, T1 = S
+			tremPrint("Aproximando de Engelberg");
+			while (true)
+			{
+				// tremPrint("Em transito");
+				if (trens->C())
+				{
+					tremPrint("Chegando em Engelberg (frear)");
+					this->ordem = "s-d";
+					break;
+				}
+				Thread::SleepMS(1); // 25 é muito, ele perde o sensor as vezes.
+			}
+
+			// Aguardar parar
+			while (tremSpeed > 0)
+			{
+				Thread::SleepMS(10);
+			}
+			tremPrint("Chegada em Engelberg (desembarque)");
+			Thread::SleepMS(10000);
+		}
+
+		else if (trans == "T1-t8")
+		{ // Saindo de engelberg
+			tremPrint("Saindo de Engelberg (embarque)");
+			Thread::SleepMS(1000); //inverteu o sleep pra esperar embarque antes de começar a ir pra left
+			this->ordem = "l";
+		}
+		
+		else if (trans == "T1-t9")
+		{ // devolve o mutex, ou seja, gate muda de valor
+			tremPrint("Aproximando de Stans");
+			while (true)
+			{
+				if (trens->B1())
+				{
+					tremPrint("Pronto, outro trem. Sua vez");
+					break;
+				}
+				Thread::SleepMS(1); // 25 é muito, ele perde o sensor as vezes.
+			}
+			Thread::SleepMS(3000);
+			//ok go ahead
+			//nao pode ter tempo pq senao buga
+		}
+		
+		else if (trans == "T1-t10")
+		{ // T1 segue viagem (está saindo do GATE e voltando pra origem!!!)
+			tremPrint("Trem 1 segue viagem");
+		}
+		
+		else if (trans == "T1-t11")
+		{ // Aparentemente T1 chegada em Lucerne, deverá parar!!!1111!!!
+			while (true)
+			{
+				if (trens->A1())
+				{
+					tremPrint("Chegando em Lucerne");
+					this->ordem = "s-e";
+					break;
+				}
+				Thread::SleepMS(1); // 25 é muito, ele perde o sensor as vezes.
+			}
+
+			Thread::SleepMS(4000);
+			tremPrint("Desembarque em Lucerne");
+			Thread::SleepMS(4000);
+			tremPrint("Aguardando comando para partida");
+		}
+
+		else if (trans == "T2-t12")
+		{ // Parar trem 2
+			this->ordem = "s";
+			tremPrint("Parar, caminho em uso");
+			Thread::SleepMS(2000); //2000ms? éh
+			//nao pode ter tempo pq senao buga
+		}
+		else if (trans == "T2-t13")
+		{ // T2 prossegue por Stans sem parar
+			trens->Gate(0);
+			tremPrint("Caminho livre, prosseguir");
+			// Thread::SleepMS(1000);
+			//nao pode ter tempo pq senao buga
+		}
+		else if (trans == "T2-t14")
+		{ // T1 teve liberação para prosseguir
+			trens->Gate(0);
+			tremPrint("Seguir, caminho liberado");
+			this->ordem = "r";
+			//nao pode ter tempo pq senao buga
+		}
+		else if (trans == "T2-t15")
+		{ // Seguir viagem
+			while (tremPos < 0)
+			{
+				Thread::SleepMS(100);
+			}
+			tremPrint("Seguindo viagem");
+		}
+		else if (trans == "T2-t16")
 		{ //
+			tremPrint("Aproximando de Engelberg");
+			while (true)
+			{
+				// tremPrint("Em transito");
+				if (trens->C())
+				{
+					tremPrint("Chegando em Engelberg (frear)");
+					this->ordem = "s-d";
+					break;
+				}
+				Thread::SleepMS(1); // 25 é muito, ele perde o sensor as vezes.
+			}
+
+			// Aguardar parar
+			while (tremSpeed > 0)
+			{
+				Thread::SleepMS(10);
+			}
+			tremPrint("Chegada em Engelberg (desembarque)");
+			Thread::SleepMS(10000);
 		}
-		else if (trans == "t1-t5")
+		else if (trans == "T2-t17")
 		{ //
+			tremPrint("Saindo de Engelberg (embarque)");
+			Thread::SleepMS(1000); //inverteu o sleep pra esperar embarque antes de começar a ir pra left
+			this->ordem = "l";
 		}
-		else if (trans == "t1-t6")
+		else if (trans == "T2-t18")
 		{ //
+			while (true)
+			{
+				if (trens->C())
+				{
+					tremPrint("Aproximando de Stans");
+					this->ordem = "s-d";
+					break;
+				}
+				Thread::SleepMS(1); // 25 é muito, ele perde o sensor as vezes.
+			}
+			
+			while (true)
+			{
+				if (trens->B2())
+				{
+					tremPrint("Pronto, outro trem. Sua vez");
+					break;
+				}
+				Thread::SleepMS(1); // 25 é muito, ele perde o sensor as vezes.
+			}
+			Thread::SleepMS(3000);
+			//ok go ahead
+			//nao pode ter tempo pq senao buga
 		}
-		else if (trans == "t1-t7")
+		else if (trans == "T2-t19")
 		{ //
+			tremPrint("Trem 2 segue viagem");
 		}
-		else if (trans == "t1-t8")
+		else if (trans == "T2-t20")
 		{ //
+			while (true)
+			{
+				if (trens->A2())
+				{
+					tremPrint("Chegando em Sarnen");
+					this->ordem = "s-e";
+					break;
+				}
+				Thread::SleepMS(1); // 25 é muito, ele perde o sensor as vezes.
+			}
+
+			Thread::SleepMS(4000);
+			tremPrint("Desembarque em Sarnen");
+			Thread::SleepMS(4000);
+			tremPrint("Aguardando comando para partida");
 		}
-		else if (trans == "t1-t9")
-		{ //
+		else if (trans == "T2-t21")
+		{ // Partir de Sarnen
+			tremPrint("Saindo da estacao Sarnen");
+			Thread::SleepMS(rand() % 1000);
+			this->ordem = "r";
+			Thread::SleepMS(1000);
 		}
-		else if (trans == "t1-t10")
+		else if (trans == "T2-t22")
 		{ //
+			while (true)
+			{
+				// printf("%d %d\n", 'g', trens->GetLastKey());
+				if (trens->GetLastKey() == 'g')
+				{
+					tremPrint("Ultima chamada (embarque)");
+					trens->GetLastKeyAndEmpty(); // limpar
+					break;
+				}
+				Thread::SleepMS((rand() % 10) + 5);
+			}
+			Thread::SleepMS(4000);
+			tremPrint("T1 partindo");
+			this->ordem = "r";
 		}
-		else if (trans == "t1-t11")
-		{ //
-		}
-		else if (trans == "t2-t12")
-		{ //
-		}
-		else if (trans == "t2-t13")
-		{ //
-		}
-		else if (trans == "t2-t14")
-		{ //
-		}
-		else if (trans == "t2-t15")
-		{ //
-		}
-		else if (trans == "t2-t16")
-		{ //
-		}
-		else if (trans == "t2-t17")
-		{ //
-		}
-		else if (trans == "t2-t18")
-		{ //
-		}
-		else if (trans == "t2-t19")
-		{ //
-		}
-		else if (trans == "t2-t20")
-		{ //
-		}
-		else if (trans == "t2-t21")
-		{ //
-		}
-		else if (trans == "t2-t22")
-		{ //
-		}
-		else if (trans == "t2-t23")
-		{ //
+		else if (trans == "T2-t23")
+		{ // Aguardar B2 ligar
+			while (true)
+			{
+				if (trens->A2())
+				{
+					tremPrint("Em transito");
+					break;
+				}
+				Thread::SleepMS(1); // 25 é muito, ele perde o sensor as vezes.
+			}
+			while (true)
+			{
+				if (trens->B2())
+				{
+					tremPrint("Aproximando de Stans");
+					break;
+				}
+				Thread::SleepMS(1); // 25 é muito, ele perde o sensor as vezes.
+			}
 		}
 	}
 
@@ -195,15 +427,17 @@ class Trem {
 		while (true){
 			// trens->Trem2Txt(rdp->printEstado());
 
-			Thread::SleepMS(2000);
+			Thread::SleepMS(rand() % 50);
 
 			rdp->lockRede();
 			trans_hab = rdp->getHabilitadoTrem(nome);
 
+			printf("Trem %s -> transição start %s\n", nome.data(), trans_hab.data());
+
 			// Caso não haja transição habilitada, desbloqueia, dorme um pouco e tenta novamente.
 			if (trans_hab == ""){
 				rdp->unlockRede();
-				tremPrint("Nada a fazer...");
+				// tremPrint("Nada a fazer...");
 				Thread::SleepMS(500);
 				continue;
 			}
@@ -218,6 +452,8 @@ class Trem {
 			rdp->finalizaTransicao(trans_hab);
 			// tremPrint("Finalizado transicao " + trans_hab + rdp->printEstado());
 			rdp->unlockRede();
+
+			// printf("Trem %s -> transição end   %s\n", nome.data(), trans_hab.data());
 		}
 		threadRunner.join();
 	}
